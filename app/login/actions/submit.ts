1aef00eac9f9da08ee2e82d121d40b6cc81d7df6
Eugen Guriev <gurievcreative@gmail.com>
2024-12-26 23:27:30 +0200

    refactor: remove commented-out code from submit action

--- app/login/actions/submit.ts
+++ app/login/actions/submit.ts
@@ -1,23 +1,8 @@
 "use server";
 
-// import { redirect } from "next/navigation";
-
-// import passSetCookie from "@/app/utils/passSetCookie";
-// import { api } from "@/lib/openapi/apiClient";
-
 import type { FormSchema } from "../zod";
 
 const submit = async (body: FormSchema) => {
-  // const request = await api.authorization("/login", "post", {
-  //   headers: { "Content-Type": "application/json" },
-  //   body,
-  // });
-
-  // const response = await request.json();
-  // passSetCookie(request.headers.getSetCookie());
-  // if (request.status === 200) {
-  //   redirect("/app");
-  // }
   return body;
 };
 
